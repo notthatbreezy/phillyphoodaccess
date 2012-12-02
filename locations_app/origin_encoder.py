6f4c894405057acab6eabcf14f394fd6929bba8a
Christopher Nies <christophernies@gmail.com>
2012-12-02 12:35:24 -0500

    trying to fix geocoder

--- locations_app/origin_encoder.py
+++ locations_app/origin_encoder.py
@@ -4,7 +4,7 @@ import json
 print 'Encoding Origin'
 
 def encode_origin(origin_address):
-	origin_address += '&components=administrative_level:'
+	origin_address += '&components=administrative_area:Philadelphia'
 	g = geocoders.Google()
 	place, (lat, lng) = g.geocode(origin_address)
 	return [lat,lng]
@@ -15,4 +15,5 @@ def reverse_encode_origin(latlon):
 	print g.reverse(coordinates)
 
 latlon = encode_origin('755 N. 26th St.')
-print latlon	
+print latlon
+#print reverse_encode_origin(latlon)
