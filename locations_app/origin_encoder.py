055831ad91629aaf5e3cbd876ee1b18497db81f6
Christopher Nies <christophernies@gmail.com>
2012-12-02 13:12:56 -0500

    fixed-ish the geocoder. Rare exception bugs that need ironed out.

--- locations_app/origin_encoder.py
+++ locations_app/origin_encoder.py
@@ -4,5 +4,6 @@ import json
 def encode_origin(origin_address):
 	origin_address += 'Philadelphia,PA'
 	g = geocoders.Google()
-	place, (lat, lng) = g.geocode(origin_address)
+	place, (lat, lng) = g.geocode(origin_address)#, exactly_one=False)
+	print lat,lng,place
 	return [lat,lng]
