22d9f56be27f3a10c70a8fd36353727a47d690e5
jwb82 <Jason@n2-151-120.dhcp.drexel.edu>
2012-12-02 14:35:31 -0500

    recipes page will redirect without a post value

--- recipes_app/views.py
+++ recipes_app/views.py
@@ -8,20 +8,25 @@ from subprocess import call
 import string
 from yummly_api import *
 from login_credentials import *
+from django.shortcuts import redirect
+
 
 def recipe_search(request):
     return render_to_response('recipe_home.html', context_instance=RequestContext(request))
 
 def recipe_results(request):
-    ingredients = request.POST['i']
-    # ingredients = ingredients.translate(string.maketrans("",""), string.punctuation)
-    yummer = YummlySearch(yummly_app_id, yummly_app_key)
-    yummer.recipe_search(ingredients)
-    yummer.get_ingredients()
-    try:
-        yummer.get_daily_percents(dv_dict)
-    except:
-        return redirect('/search_again/')
-    # yummer.sort_recipes('ENERC_KCAL')
-    # yummer.sorted_recipes_new()
-    return render_to_response('recipe_results.html', {"recipes":yummer.recipes}, context_instance=RequestContext(request))
+    if request.POST:
+        ingredients = request.POST['i']
+        # ingredients = ingredients.translate(string.maketrans("",""), string.punctuation)
+        yummer = YummlySearch(yummly_app_id, yummly_app_key)
+        yummer.recipe_search(ingredients)
+        yummer.get_ingredients()
+        try:
+            yummer.get_daily_percents(dv_dict)
+        except:
+            return redirect('/search_again/')
+        # yummer.sort_recipes('ENERC_KCAL')
+        # yummer.sorted_recipes_new()
+        return render_to_response('recipe_results.html', {"recipes":yummer.recipes}, context_instance=RequestContext(request))
+    else:
+        return redirect('/')
