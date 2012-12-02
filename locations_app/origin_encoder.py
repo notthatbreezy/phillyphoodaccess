7c59620ed7eb26df9815d5b13e05213050ddf5e2
Christopher Nies <christophernies@gmail.com>
2012-12-02 12:29:02 -0500

    adding docs

--- locations_app/origin_encoder.py
+++ locations_app/origin_encoder.py
@@ -1,10 +1,18 @@
-from geopy import geocoders 
+from geopy import geocoders
+import json
 
 print 'Encoding Origin'
 
 def encode_origin(origin_address):
+	origin_address += '&components=administrative_level:'
 	g = geocoders.Google()
 	place, (lat, lng) = g.geocode(origin_address)
 	return [lat,lng]
-	
-print encode_origin('755 N. 26th St. Philadelphia PA 19130')
+
+def reverse_encode_origin(latlon):
+	coordinates = str(latlon[0]) + ',' + str(latlon[1]) + '&components=administrative_area:Philadelphia'
+	g = geocoders.Google()
+	print g.reverse(coordinates)
+
+latlon = encode_origin('755 N. 26th St.')
+print latlon	
