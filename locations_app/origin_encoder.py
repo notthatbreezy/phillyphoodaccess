94201d6341b5b7ca1ec828d02b00cec40e4992aa
Christopher Nies <christophernies@gmail.com>
2012-12-02 13:02:11 -0500

    fixed geocoding

--- locations_app/origin_encoder.py
+++ locations_app/origin_encoder.py
@@ -1,19 +1,10 @@
 from geopy import geocoders
 import json
 
-print 'Encoding Origin'
-
 def encode_origin(origin_address):
-	origin_address += '&components=administrative_area:Philadelphia'
+	origin_address += 'Philadelphia,PA'
 	g = geocoders.Google()
 	place, (lat, lng) = g.geocode(origin_address)
 	return [lat,lng]
-
-def reverse_encode_origin(latlon):
-	coordinates = str(latlon[0]) + ',' + str(latlon[1]) + '&components=administrative_area:Philadelphia'
-	g = geocoders.Google()
-	print g.reverse(coordinates)
-
-latlon = encode_origin('755 N. 26th St.')
-print latlon
-#print reverse_encode_origin(latlon)
+	
+print encode_origin('1800 Market St. Philadelphia PA')
